e658dc5760f184efb057f8b82bdf073513b7b42b
Chuan De Sheng <dchuan001@e.ntu.edu.sg>
2019-07-25 16:40:06 +0800

    organizers have chatID registered too on start

--- botActions/botStart.js
+++ botActions/botStart.js
@@ -10,28 +10,37 @@ const fbFunc = require('../firebaseFunctions.js');
       let username = ctx.meta.user.username;
       let charID = ctx.meta.user.id;
       let dbContainsUser;
-      fbFunc.checkIfusernameExists(db,username).then(function(res) {
-          console.log('this is the result: ',res);
-          if(res) {
-              const {chatID} = res;
-              if (chatID.length === 0) {
-                //add chatID into database
-                console.log(username)
-                fbFunc.addIdToDatabase(db, username, charID).then((res) => {
-                  ctx.sendMessage('Your username has been registered.')
-                })
-                .catch((err) => {
-                  console.log(err)
-                  ctx.sendMessage('Error in processing your data, try again.')
-                })
-              }
-          }
-          else {
-            ctx.sendMessage('Hello <%user.first_name%>, welcome to codeIT Suisse. You have not registered for the competition yet.')
-              //TODO: inform user that they have accessed the chatbot.
-              //Message: welcome to codeIT Suisse. Please register for the competition via: link
-          }
-      });
+      const data = fbFunc.checkIfusernameExists(db, username).then(({data, role}) => {
+          const {chatID} = data;
+          if (chatID.length === 0) {
+              //add chatID into database
+              fbFunc.addIdToDatabase(db, username, role, charID).then(res => {
+                ctx.sendMessage(`Hello ${role}, your username has been registered.`)
+              })
+            }
+      })
+      // fbFunc.checkIfusernameExists(db,username).then(function(res) {
+      //     console.log('this is the result: ',res);
+      //     if(res) {
+      //         const {chatID} = res;
+      //         if (chatID.length === 0) {
+      //           //add chatID into database
+      //           console.log(username)
+      //           fbFunc.addIdToDatabase(db, username, charID).then((res) => {
+      //             ctx.sendMessage('Your username has been registered.')
+      //           })
+      //           .catch((err) => {
+      //             console.log(err)
+      //             ctx.sendMessage('Error in processing your data, try again.')
+      //           })
+      //         }
+      //     }
+      //     else {
+      //       ctx.sendMessage('Hello <%user.first_name%>, welcome to codeIT Suisse. You have not registered for the competition yet.')
+      //         //TODO: inform user that they have accessed the chatbot.
+      //         //Message: welcome to codeIT Suisse. Please register for the competition via: link
+      //     }
+      // });
 
     // Invoke callback must return promise.
     return ctx.sendMessage('Hello <%=user.first_name%>. How are you?');
--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -1,38 +1,72 @@
 const serviceAccount = require('./config/serviceAccountKey.json');
 const firebase = require('firebase-admin');
 
-module.exports.checkIfusernameExists = (db,username) => {
-    return new Promise((resolve, reject) => {
-        let cityRef = db.collection('participants').doc(username);
-        let getDoc = cityRef.get()
-          .then(doc => {
-            if (!doc.exists) {
-              console.log('No such document!');
-              resolve(null);
-            } else {
-              console.log('Document data:', doc.data());
-              resolve(doc.data());
-            }
-          })
-          .catch(err => {
-            console.log('Error getting document', err);
-          });
-        return getDoc;
-    })
+module.exports.checkIfusernameExists = async (db,username) => {
+    const participantRef = db.collection('participants').doc(username);
+    let getDoc = await participantRef.get()
+    if (!getDoc.exists) {
+      // check if user is organiser
+      const orgRef = db.collection('organisers').doc(username);
+      let orgDoc = await orgRef.get();
+      if (!orgDoc.exists) {
+        return null;
+      } else {
+        const data = orgDoc.data();
+
+        return {data, role:'organiser'};
+      }
+    } else {
+      const data = getDoc.data();
+      return {data, role: 'participant'};
+    }
+    // return new Promise((resolve, reject) => {
+    //     const participantRef = db.collection('participants').doc(username);
+    //     const getDoc = participantRef.get()
+    //       .then(doc => {
+    //         if (!doc.exists) {
+    //           //check if user is organiser
+    //           let orgRef = db.collection('organisers').doc(username);
+    //           const orgDoc = orgRef.get()
+    //           .then( doc => {
+  //                if (!doc.exists)
+  //              })
+    //
+    //           console.log('No such document!');
+    //           resolve(null);
+    //         } else {
+    //           console.log('Document data:', doc.data());
+    //           resolve(doc.data());
+    //         }
+    //       })
+    //       .catch(err => {
+    //         console.log('Error getting document', err);
+    //       });
+    //     return getDoc;
+    // })
 
 }
 
-module.exports.addIdToDatabase = (db, name, charID) => {
-  return new Promise((resolve, reject) => {
-    let cityRef = db.collection('participants').doc(name);
 
-    return cityRef.update({
-      chatID: charID
-    }).then(() => resolve(true))
-    .catch((err) => {
-      console.log(err)
-      resolve(false)
-    });
+module.exports.addIdToDatabase = async (db, name, role, charID) => {
+  let docRef = db.collection(`${role}s`).doc(name);
+  try {
+    let updated = await docRef.update({chatID: charID});
+    return updated;
+  } catch(err) {
+    console.log(err);
+    return err;
+  }
 
-  })
+  // return new Promise((resolve, reject) => {
+  //   let cityRef = db.collection(role).doc(name);
+  //
+  //   return cityRef.update({
+  //     chatID: charID
+  //   }).then(() => resolve(true))
+  //   .catch((err) => {
+  //     console.log(err)
+  //     resolve(false)
+  //   });
+  //
+  // })
 }
